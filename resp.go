1658b8321775366f7c0e4d896796fce21052b7ad
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-07-26 16:24:39 +0530

    read each line from the byte

---
+++ resp.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"log"
+)
+
+const (
+	STRING  = '+'
+	ERROR   = '-'
+	INTEGER = ':'
+	BULK    = '$'
+	ARRAY   = '*'
+)
+
+type Value struct {
+	typ   string
+	str   string
+	num   int
+	bulk  string
+	array []Value
+}
+
+type Resp struct {
+	reader *bufio.Reader
+}
+
+func NewResp(rd io.Reader) *Resp {
+	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r *Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			log.Println("Error in readline func, reading bytes")
+			return nil, 0, err
+		}
+
+		n += 1
+		line = append(line, b)
+
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+	}
+
+	return line[:len(line)-1], n, nil
+}
